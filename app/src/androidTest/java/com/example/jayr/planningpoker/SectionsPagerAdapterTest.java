4f9f99de3cdb244ad4cdade5958ff6209e3d56d1
jay <jay@reutter.de>
2016-09-25 00:24:33 +0200

    init
    Signed-off-by: jay <jay@reutter.de>

---
+++ app/src/androidTest/java/com/example/jayr/planningpoker/SectionsPagerAdapterTest.java
@@ -0,0 +1,22 @@
+package com.example.jayr.planningpoker;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by JAYR on 27.08.2016.
+ */
+public class SectionsPagerAdapterTest extends TestCase {
+
+
+    public void testGetItem() throws Exception {
+
+    }
+
+    public void testGetCount() throws Exception {
+
+    }
+
+    public void testGetPageTitle() throws Exception {
+
+    }
+}
